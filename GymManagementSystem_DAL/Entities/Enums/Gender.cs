ec0b97d55cf61c7d3600a17145ba3121f22c00b9
Taha Manshoor Dar <tahadar221177@gmail.com>
2026-06-24 13:41:56 +0500

    Add BaseEntity, Gender and Specialties enums

---
+++ GymManagementSystem_DAL/Entities/Enums/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymManagementSystem_DAL.Entities.Enums
+{
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2,
+        Other = 3
+    }
+}
