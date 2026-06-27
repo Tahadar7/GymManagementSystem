c855d558d32fde35093653145d326063acc3d123
Taha Manshoor Dar <tahadar221177@gmail.com>
2026-06-27 20:10:06 +0500

    Added missing GymDBContext

--- GymManagementSystem_DAL/Data/DBContexts/GymDBContext.cs
+++ GymManagementSystem_DAL/Data/DBContexts/GymDBContext.cs
@@ -1,10 +1,43 @@
-﻿using System;
+﻿using GymManagementSystem_DAL.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace GymManagementSystem_DAL.Data.DBContexts
 {
-    internal class GymDBContext
+    public class GymDBContext(DbContextOptions<GymDBContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Applies every IEntityTypeConfiguration<T> class in this assembly
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            // ApplicationUser is configured here directly
+            modelBuilder.Entity<ApplicationUser>(Eb => 
+            {
+                Eb.Property(X => X.FirstName)
+                  .HasColumnType("varchar")
+                  .HasMaxLength(50);
+                Eb.Property(X => X.LastName)
+                  .HasColumnType("varchar")
+                  .HasMaxLength(50);
+            });
+        }
+
+        #region Db Sets
+        public DbSet<Member> Members { get; set; } = null!;
+        public DbSet<Trainer> Trainers { get; set; } = null!;
+        public DbSet<Plan> Plans { get; set; } = null!;
+        public DbSet<MemberShip> MemberShips { get; set; } = null!;
+        public DbSet<Session> Sessions { get; set; } = null!;
+        public DbSet<MemberSession> MemberSessions { get; set; } = null!;
+        public DbSet<HealthRecord> HealthRecords { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+        #endregion
     }
 }
